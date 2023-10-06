defcd8750b9374bf2633e3dcfa0b966ecac0eb61
Keutaby <141675564+Keutaby@users.noreply.github.com>
2023-10-06 13:34:35 -0600

    Update open.php

--- open.php
+++ open.php
@@ -1,6 +1,10 @@
 <?php
+//database: id20310606_registro
+//user: id20310606_id217212_registro
+//host: localhost
+//password: ID217212_registro
 
-    $conexion = new mysqli("localhost", "id20310606_id217212_registro", "ID217212_registro", "personas");
+    $conexion = new mysqli("localhost", "id20310606_id217212_registro", "ID217212_registro", "id20310606_registro");
     if($conexion){
         echo "conexion exitosa";
     } else{
